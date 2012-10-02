58e054b8e1e7d40c8519122989c149bb13ae83c5
jw84 <jerrywang@me.com>
2012-10-01 22:32:51 -0700

    added more html5 code

--- alfie/urls.py
+++ alfie/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
+from django.views.static import *
 from django.views.generic import TemplateView
-
+from django.conf import settings
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
@@ -17,3 +18,10 @@ urlpatterns = patterns('',
 urlpatterns += patterns('',
 	url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+	    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.MEDIA_ROOT,
+    	}),
+	)
