5a5de45298e7eb22df3e2d8aec99d96922f8c66f
Jerry Wang <jw@jinx.local>
2012-10-15 10:38:44 -0700

    stashing2

--- alfie/apps/sales/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url, include
-
-# See: https://docs.djangoproject.com/en/dev/topics/http/urls/
-urlpatterns = patterns('alfie.apps.sales.views',
-    # show a list of trailers
-    url(r'^', 'index', name='sales_index'),
-)
