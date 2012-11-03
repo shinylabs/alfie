1857c9570059d70f0caa9dcaf73f8ba6899d24da
Jerry Wang <jw@jinx.local>
2012-11-02 19:12:19 -0700

    two forms in one

--- alfie/apps/orders/views.py
+++ alfie/apps/orders/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from alfie.apps.orders.forms import OrderForm
+from alfie.apps.users.forms import SignupFormExtra
 
 def showMenu(request):
 	"""
@@ -10,14 +11,16 @@ def showMenu(request):
 		Input: selection
 		Output: modelform
 	"""
+	choice = ''
 	if request.method == 'POST': # If the form has been submitted...
 		form = OrderForm(request.POST) # A form bound to the POST data
-		if form.is_valid():
-			plan_selection = form.data['menu']
-			return HttpResponse('you chose plan %s -> now register <a href="/accounts/signup">user form</a>' % plan_selection) # Redirect after POST
+		choice = form.data['menu']
+		form = SignupFormExtra()
+#		return HttpResponse('you chose %s -> now register <a href="/accounts/signup">user form</a>' % choice) # Redirect after POST
 	else:
-		form = OrderForm() # An unbound form
-	return render_to_response('orders/order_form.html', {'form': form,}, context_instance=RequestContext(request))
+		orderform = OrderForm() # An unbound form
+		signupform = SignupFormExtra()
+	return render_to_response('orders/order_form.html', {'orderform': orderform, 'signupform': signupform, 'choice': choice}, context_instance=RequestContext(request))
 
 def startOrder(request):
 	"""
@@ -42,6 +45,6 @@ def saveOrder(request):
 
 ORDER FLOW
 
-index -> order form -> register form / payment form -> pref form -> success page -> success email
+index -> order form / register form / shipping form / payment form / preferences form -> success page -> success email
 
 """
