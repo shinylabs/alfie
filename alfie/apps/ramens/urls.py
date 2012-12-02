af5bcd7c85de18edd727628ef30580f806ed539d
Jerry Wang <jw@jinx.local>
2012-12-01 20:52:31 -0800

    mfg generic views

--- alfie/apps/ramens/urls.py
+++ alfie/apps/ramens/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url, include
-from alfie.apps.ramens.views import RamenListView, RamenDetailView, RamenCreateView, RamenUpdateView, RamenDeleteView, MfgListView
+from alfie.apps.ramens.views import *
 
 urlpatterns = patterns('',
     url(r'^ramen/$', RamenListView.as_view(), name='ramen_list'),
@@ -7,5 +7,10 @@ urlpatterns = patterns('',
     url(r'^ramen/(?P<pk>\d+)/$', RamenDetailView.as_view(), name='ramen_detail'),
     url(r'^ramen/(?P<pk>\d+)/update/$', RamenUpdateView.as_view(), name='ramen_update'),
     url(r'^ramen/(?P<pk>\d+)/delete/$', RamenDeleteView.as_view(), name='ramen_delete'),
-    url(r'^ramen/mfg/$', MfgListView.as_view()),
+
+    url(r'^ramen/mfg/$', MfgListView.as_view(), name='mfg_list'),
+    url(r'^ramen/mfg/add/$', MfgCreateView.as_view(), name='mfg_add'),
+    url(r'^ramen/mfg/(?P<pk>\d+)/$', MfgDetailView.as_view(), name='mfg_detail'),
+    url(r'^ramen/mfg/(?P<pk>\d+)/update/$', MfgUpdateView.as_view(), name='mfg_update'),
+    url(r'^ramen/mfg/(?P<pk>\d+)/delete/$', MfgDeleteView.as_view(), name='mfg_delete'),
 ) 
--- alfie/apps/ramens/views.py
+++ alfie/apps/ramens/views.py
@@ -26,13 +26,20 @@ class MfgListView(ListView):
 	context_object_name = 'mfg_list'
 	template_name = 'ramens/mfg_list.html'
 
+class MfgCreateView(CreateView):
+	model = Manufacturer
+	template_name = 'ramens/mfg_form.html'
+
 class MfgDetailView(DetailView):
 	queryset = Manufacturer.objects.all()
 	context_object_name = 'mfg'
+	template_name = 'ramens/mfg_detail.html'
 
-	def get_context_data(self, **kwargs):
-		# Call the base implementation first to get a context
-		context = super(MfgDetail, self).get_context_data(**kwargs)
-		# Add in the QuerySet of all the books
-		context['ramen_list'] = Ramen.objects.all()
-		return context
+class MfgUpdateView(UpdateView):
+	model = Manufacturer
+	success_url = reverse_lazy('mfg_list')
+	template_name = 'ramens/mfg_form.html'
+
+class MfgDeleteView(DeleteView):
+	model = Manufacturer
+	success_url = reverse_lazy('mfg_list')
