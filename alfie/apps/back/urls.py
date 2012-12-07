eabf25839f874796020f11849fc9ceb66e477968
Jerry Wang <jw@jinx.local>
2012-12-06 18:45:13 -0800

    backoffice app

--- alfie/apps/back/urls.py
+++ alfie/apps/back/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url, include
 
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('alfie.apps.back.views',
-   	url(r'^back/$', 'index', name="index"),
+   	url(r'^office/$', 'index', name="index"),
 )
--- alfie/apps/back/views.py
+++ alfie/apps/back/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import TemplateView
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.db.models import Sum
+#bigups http://stackoverflow.com/questions/5757094/decimal-zero-padding
+from util import moneyfmt
+
+from alfie.apps.profiles.models import Profile
+from alfie.apps.orders.models import Order
 
 def index(request):
-	return render_to_response('back/index.html', context_instance=RequestContext(request))
+	profiles = Profile.objects.all()
+	inv_count = Order.objects.aggregate(Sum('choice__size')).values()[0]
+	total_rev = moneyfmt(Order.objects.aggregate(Sum('choice__price')).values()[0], curr='$')
+	return render_to_response('back/index.html', {'profiles': profiles, 'inv_count': inv_count, 'total_rev': total_rev}, context_instance=RequestContext(request))
